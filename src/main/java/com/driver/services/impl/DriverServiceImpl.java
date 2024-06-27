6b5f30c65a007b95faebc78cf8decdb022700e57
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-06-27 09:37:23 +0530

    uber

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -9,30 +9,52 @@ import org.springframework.stereotype.Service;
 import com.driver.model.Driver;
 import com.driver.repository.DriverRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class DriverServiceImpl implements DriverService {
 
-	@Autowired
-	DriverRepository driverRepository3;
+    @Autowired
+    DriverRepository driverRepository3;
 
-	@Autowired
-	CabRepository cabRepository3;
+    @Autowired
+    CabRepository cabRepository3;
 
-	@Override
-	public void register(String mobile, String password){
-		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+    @Override
+    public void register(String mobile, String password){
+        //Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+        Cab cab = new Cab(10,true);
+        Driver driver = new Driver(mobile,password);
 
-	}
+        cab.setDriver(driver);
+        driver.setCab(cab);
 
-	@Override
-	public void removeDriver(int driverId){
-		// Delete driver without using deleteById function
+        driverRepository3.save(driver);
 
-	}
+    }
 
-	@Override
-	public void updateStatus(int driverId){
-		//Set the status of respective car to unavailable
+    @Override
+    public void removeDriver(int driverId){
+        // Delete driver without using deleteById function
+        if(driverRepository3.findById(driverId).isPresent()){
+            Driver driver = driverRepository3.findById(driverId).get();
 
-	}
+            driverRepository3.delete(driver);
+        }
+
+    }
+
+    @Override
+    public void updateStatus(int driverId){
+        //Set the status of respective car to unavailable
+        if(driverRepository3.findById(driverId).isPresent()){
+            Driver driver = driverRepository3.findById(driverId).get();
+            Cab cab = driver.getCab();
+            cab.setAvailable(false);
+
+            driverRepository3.save(driver);
+        }
+
+    }
 }
